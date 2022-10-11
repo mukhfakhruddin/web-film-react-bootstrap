7b2f47a9da5d62d6df9f278c8b5f72bbc1c1a14d
unknown <mukhfakhruddin@gmail.com>
2022-10-11 08:17:41 +0700

    landing page react bootstrap

---
+++ src/components/Superhero.js
@@ -0,0 +1,93 @@
+import { Card, Container, Row, Col, Image } from "react-bootstrap";
+import antmanImage from "../assets/images/superhero/antman.jpg";
+import avengerImage from "../assets/images/superhero/avenger.jpg";
+import batmanImage from "../assets/images/superhero/batman.jpg";
+import robinhoodImage from "../assets/images/superhero/robinhood.jpg";
+import spidermanImage from "../assets/images/superhero/spiderman-cover.jpg";
+import supermanImage from "../assets/images/superhero/superman.jpg";
+
+const SuperHero = () => {
+  return (
+    <div>
+      <Container>
+        <h1 className="text-white">SUPERHERL MOVIES</h1>
+        <Row>
+          <Col md={4} className="movieWrapper" id="superhero">
+            <Card className="movieImage ">
+              <Image src={antmanImage} alt="Antman Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">ANTMAN</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage ">
+              <Image src={avengerImage} alt="Avenger Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">AVENGER</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage ">
+              <Image src={batmanImage} alt="Batman Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">BATMAN</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage ">
+              <Image src={robinhoodImage} alt="Robinhood Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">ROBIN HOOD</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage ">
+              <Image src={spidermanImage} alt="Spiderman Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">SPIDERMAN</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+          <Col md={4} className="movieWrapper">
+            <Card className="movieImage ">
+              <Image src={supermanImage} alt="Superman Movies" className="images" />
+              <div className="bg-dark">
+                <div className="text-white p-2 m-1">
+                  <Card.Title className="text-center">SUPERMAN</Card.Title>
+                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content</Card.Text>
+                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                </div>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default SuperHero;
